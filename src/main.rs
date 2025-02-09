7f86549d7d5919ac17d23ebb331c9bdf6b760256
Pybounce <pybounce@protonmail.com>
2025-02-09 14:24:29 +0000

    using arc<file> to write on multiple threads but also it's not thread safe so

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
 use std::{
-    error, fs::File, io::Write, sync::Arc, time::Instant
+    error, fs::File, io::Write, os::unix::fs::FileExt, sync::Arc, thread, time::{self, Instant}
 };
 
-use bytes::{Bytes, BytesMut};
 use reqwest::{self, Client};
 use tokio::spawn;
 
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use futures_util::StreamExt;
 
-const DOWNLOAD_URL: &str = "https://testfile.org/files-5GB";
+const DOWNLOAD_URL: &str = "http://speedtest.tele2.net/1GB.zip";
 const FILE_NAME: &str = "file.zip";
 
 
 async fn linear_download() -> Result<(), Box<dyn error::Error>> {
-    let response = reqwest::get(DOWNLOAD_URL).await?
-    .bytes()
-    .await?;
-    
-    let mut downloaded_file = File::create(FILE_NAME)?;
-    downloaded_file.write_all(&response)?;
+    let mut file = File::create(FILE_NAME)?;
+    let response = reqwest::get(DOWNLOAD_URL).await?;
+    let mut stream = response.bytes_stream();
+    let mut offset = 0;
+    while let Some(item) = stream.next().await {
+        let chunk = item.unwrap();
+        let _ = file.write_at(&chunk, offset);
+        offset += chunk.len() as u64;
+    }
+    file.flush().unwrap();
     Ok(())
 }
 
@@ -43,7 +46,8 @@ async fn download() -> Result<(), Box<dyn error::Error>> {
         if let Some(header) = headers.get("content-length") {
             let content_length: usize = header.to_str()?.parse::<usize>()?;
             let chunk_size: usize = (content_length / 6) + 1;
-            let mut tasks: Vec<tokio::task::JoinHandle<Result<Bytes, ()>>> = Vec::with_capacity(content_length / chunk_size);
+            let mut tasks: Vec<tokio::task::JoinHandle<Result<(), ()>>> = Vec::with_capacity(content_length / chunk_size);
+            let mut file = Arc::new(File::create(FILE_NAME)?);
 
             for start_byte in (0..content_length).step_by(chunk_size) {
                 let end_byte = (start_byte + chunk_size).min(content_length);
@@ -52,17 +56,14 @@ async fn download() -> Result<(), Box<dyn error::Error>> {
                 pb.set_message("downloading...");
                 pb.set_style(sty.clone());
                 pb.set_position(0);
-                tasks.push(spawn(download_range(Arc::clone(&client), start_byte, end_byte - 1, Arc::new(pb))));
+                tasks.push(spawn(download_range(Arc::clone(&client), start_byte, end_byte - 1, Arc::new(pb), file.clone())));
             }
-
-            let mut bytes: Vec<u8> = Vec::with_capacity(content_length);
 
             for task in tasks.iter_mut() {
-                let a = task.await?.unwrap();
-                bytes.extend(a);
+                let _ = task.await?.unwrap();
+                //handle err returns here
             }
-            let mut downloaded_file = File::create(FILE_NAME)?;
-            downloaded_file.write_all(&bytes)?;
+            file.flush().unwrap();
         }
         println!("written with chunks");
     }
@@ -74,22 +75,24 @@ async fn download() -> Result<(), Box<dyn error::Error>> {
     Ok(())
 }
 
-async fn download_range(client: Arc<Client>, start_byte: usize, end_byte: usize, pb: Arc<ProgressBar>) -> Result<Bytes, ()> {
+async fn download_range(client: Arc<Client>, start_byte: usize, end_byte: usize, pb: Arc<ProgressBar>, file: Arc<File>) -> Result<(), ()> {
     let range = format!("bytes={}-{}", start_byte, end_byte);
     
     if let Ok(response) = client.get(DOWNLOAD_URL).header("Range", range).send().await {
 
         let mut stream = response.bytes_stream();
-        let mut bytes = BytesMut::new();
+        let mut download_offset = 0;
 
         while let Some(item) = stream.next().await {
             let chunk = item.unwrap();
-            pb.inc(chunk.len() as u64);
-            bytes.extend_from_slice(&chunk);
+            let _ = file.write_at(&chunk, start_byte as u64 + download_offset);
+            download_offset += chunk.len() as u64;
+            pb.set_position(download_offset);
+
         }
         pb.finish_with_message("done");
 
-        return Ok(bytes.freeze());
+        return Ok(());
     }
     
     return Err(());
