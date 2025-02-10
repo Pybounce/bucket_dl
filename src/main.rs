d778492ab30cc33393c53dd28193e25029ca85c1
Pybounce <pybounce@protonmail.com>
2025-02-10 00:59:02 +0000

    cleaning

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    error, fs::{File, OpenOptions}, io::{Read, Seek, Write}, os::unix::fs::FileExt, sync::Arc, thread, time::{self, Instant}
+    error, fs::{File, OpenOptions}, io::{Seek, Write}, sync::Arc, time::Instant
 };
 
 use reqwest::{self, Client};
@@ -11,27 +11,11 @@ use futures_util::StreamExt;
 const DOWNLOAD_URL: &str = "http://speedtest.tele2.net/1GB.zip";
 const FILE_NAME: &str = "file.zip";
 
-
-async fn linear_download() -> Result<(), Box<dyn error::Error>> {
-    let mut file = File::create(FILE_NAME)?;
-    let response = reqwest::get(DOWNLOAD_URL).await?;
-    let mut stream = response.bytes_stream();
-    let mut offset = 0;
-    while let Some(item) = stream.next().await {
-        let chunk = item.unwrap();
-        let _ = file.write_at(&chunk, offset);
-        offset += chunk.len() as u64;
-    }
-    file.flush().unwrap();
-    Ok(())
-}
-
-
 async fn download() -> Result<(), Box<dyn error::Error>> {
 
     let mp = MultiProgress::new();
     let sty = ProgressStyle::with_template(
-        "[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}",
+        "[{elapsed_precise}] {bar:40.cyan/blue} {bytes}/{total_bytes} ({eta})",
     )
     .unwrap()
     .progress_chars("##-");
@@ -41,36 +25,39 @@ async fn download() -> Result<(), Box<dyn error::Error>> {
     let client = Arc::new(Client::new());
     let head_response = client.head(DOWNLOAD_URL).send().await?;
     let headers = head_response.headers();
+    let content_length: usize = headers.get("content-length").unwrap().to_str()?.parse::<usize>()?;
+    let chunk_size: usize = get_chunk_size(content_length, headers.contains_key("accept-ranges"));
 
-    if headers.contains_key("accept-ranges") {
-        if let Some(header) = headers.get("content-length") {
-            let content_length: usize = header.to_str()?.parse::<usize>()?;
-            let chunk_size: usize = (content_length / 6) + 1;
-            let mut tasks: Vec<tokio::task::JoinHandle<Result<(), ()>>> = Vec::with_capacity(content_length / chunk_size);
-            let file = File::create(FILE_NAME)?;
+    let mut tasks: Vec<tokio::task::JoinHandle<Result<(), ()>>> = Vec::with_capacity(content_length / chunk_size);
+    let _file = File::create(FILE_NAME)?;
 
-            for start_byte in (0..content_length).step_by(chunk_size) {
-                let end_byte = (start_byte + chunk_size).min(content_length);
+    for start_byte in (0..content_length).step_by(chunk_size) {
+        let end_byte = (start_byte + chunk_size).min(content_length);
 
-                let pb = mp.add(ProgressBar::new((end_byte - start_byte) as u64));
-                pb.set_message("downloading...");
-                pb.set_style(sty.clone());
-                pb.set_position(0);
-                tasks.push(spawn(download_range(Arc::clone(&client), start_byte, end_byte - 1, Arc::new(pb))));
-            }
-
-            for task in tasks.iter_mut() {
-                let _ = task.await?.unwrap();
-                //handle err returns here
-            }
-        }
+        let pb = mp.add(ProgressBar::new((end_byte - start_byte) as u64));
+        pb.set_style(sty.clone());
+        pb.set_position(0);
+        
+        tasks.push(spawn(download_range(Arc::clone(&client), start_byte, end_byte - 1, Arc::new(pb))));
     }
-    else { 
 
-        println!("does not accept range"); 
+    for task in tasks.iter_mut() {
+        let _ = task.await?.unwrap();
     }
-    
+
+
     Ok(())
+}
+
+fn get_chunk_size(content_length: usize, accepts_ranges: bool) -> usize {
+    if accepts_ranges == false { return content_length; }
+    return (content_length / 6) + 1;
+}
+
+fn undo_all() {
+    println!("Deleting unfinished file...");
+    std::fs::remove_file(FILE_NAME).unwrap();
+    println!("Deleted unfinished file.");
 }
 
 async fn download_range(client: Arc<Client>, start_byte: usize, end_byte: usize, pb: Arc<ProgressBar>) -> Result<(), ()> {
@@ -89,13 +76,12 @@ async fn download_range(client: Arc<Client>, start_byte: usize, end_byte: usize,
 
             let _ = file.write(&chunk).unwrap();
 
-            //let _ = file.write_at(&chunk, start_byte as u64 + download_offset);
             download_offset += chunk.len() as u64;
             pb.set_position(download_offset);
 
         }
         file.flush().unwrap();
-        pb.finish_with_message("done");
+        pb.finish();
 
         return Ok(());
     }
@@ -103,10 +89,9 @@ async fn download_range(client: Arc<Client>, start_byte: usize, end_byte: usize,
     return Err(());
 }
 
-use std::fs;
 fn compare() -> Result<(), Box<dyn std::error::Error + 'static>> {
-    let threaded = fs::read_to_string(FILE_NAME).unwrap();
-    let norm = fs::read_to_string("1GB.zip").unwrap();
+    let threaded = std::fs::read_to_string(FILE_NAME).unwrap();
+    let norm = std::fs::read_to_string("1GB.zip").unwrap();
     assert_eq!(threaded, norm);
     Ok(())
 }
@@ -129,6 +114,7 @@ async fn main() {
         },
         Err(x) => {
             println!("error: {:?}", x);
+            undo_all();
         },
     }
     
