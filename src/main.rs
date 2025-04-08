699b2520af358adde1960ddd7cf0244e28af891f
Pybounce <pybounce@protonmail.com>
2025-04-08 20:48:57 +0100

    sleep

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 
+use std::time::Duration;
+
 use futures_util::StreamExt;
 use multithreaded_download_manager::{download_client::DownloadClient, models::DownloadStatus};
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
@@ -37,8 +39,6 @@ async fn main() {
 
             if let Ok(mut client) = DownloadClient::init(&url, &file_path) {
                 if let Ok(_) = client.begin_download().await {
-
-                    //let _ = tokio::time::sleep(Duration::from_millis(1000)).await;
                     let bucket_sizes = client.bucket_sizes();
 
                     let mut progress_bars = Vec::<ProgressBar>::with_capacity(bucket_sizes.len());
@@ -73,7 +73,7 @@ async fn main() {
                 }
 
             }
-
+            let _ = tokio::time::sleep(Duration::from_millis(1000)).await;
             println!("DONE");
         },
         Err(_) => {
